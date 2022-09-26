085b04122f8ba2e24751826666dd617c48b80204
Alex <ag.perez0500@gmail.com>
2022-09-25 19:19:04 -0500

    did the db thing

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,21 @@ var express = require('express');
 var router = express.Router(); 
 
 var pool = require('../db/database');
+var client = require('../db/database');
 
 /* GET home page. */
+
+version = ''
+client.connect()
+client.query('SELECT VERSION();', (err, res) => {
+	client.end();
+	version =  res.rows;
+	console.log(version);
+})
+
 router.get('/', function(req, res, next) {
-	
-	res.render('index', { title: "Alex Perez' HW3" });
-	res.render('index', { thing: "This is a test" });
+	//console.log(version);
+	res.render('index', { title: "Alex Perez' HW3", version: version[0].version});
 });
 
 module.exports = router;
